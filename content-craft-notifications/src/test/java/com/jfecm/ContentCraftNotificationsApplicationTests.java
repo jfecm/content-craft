22e50956e81befcceafcdced3dbe705440ac9d38
joaquincorimayo <joaquin97corimayo@gmail.com>
2024-02-13 17:31:39 -0300

    feat: Add Notifications module.

---
+++ content-craft-notifications/src/test/java/com/jfecm/ContentCraftNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfecm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentCraftNotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
