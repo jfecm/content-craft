1d1800f0bb32ea6d5c62e2c05a281755bafa9b9c
joaquincorimayo <joaquin97corimayo@gmail.com>
2024-02-20 00:23:35 -0300

    feat(comments): Configure Kafka producer for sending comment messages.

---
+++ content-craft-comments/src/main/java/com/jfecm/adapters/outbound/kafka/CommentProducerConfig.java
@@ -0,0 +1,49 @@
+package com.jfecm.adapters.outbound.kafka;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class CommentProducerConfig {
+
+    public static final int NUM_PARTITIONS = 5;
+    public static final short REPLICATION_FACTOR = (short) 1;
+    public static final String SERVERS_CONFIG = "localhost:9092";
+    @Value("${kafka.topic.name}")
+    public String TOPIC_NAME;
+
+    @Bean
+    public NewTopic getTopic() {
+        return new NewTopic(TOPIC_NAME, NUM_PARTITIONS, REPLICATION_FACTOR);
+    }
+
+    @Bean
+    public Map<String, Object> producerConfig() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, SERVERS_CONFIG);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return props;
+    }
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfig());
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
--- content-craft-comments/src/main/java/com/jfecm/adapters/outbound/repository/CommentReactiveMongoAdapter.java
+++ content-craft-comments/src/main/java/com/jfecm/adapters/outbound/repository/CommentReactiveMongoAdapter.java
@@ -6,26 +6,55 @@ import com.jfecm.application.ports.out.IRetrieveCommentsPort;
 import com.jfecm.application.ports.out.ISaveCommentPort;
 import com.jfecm.application.core.dto.response.CommentCreateResponse;
 import com.jfecm.application.core.dto.response.CommentListResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Component
+@Slf4j
 public class CommentReactiveMongoAdapter implements ISaveCommentPort, IRetrieveCommentsPort {
 
     private final CommentReactiveMongoRepository commentRepository;
+    private final KafkaTemplate<String,Object> template;
+    @Value("${kafka.topic.name}")
+    public String TOPIC_NAME;
 
     @Autowired
-    public CommentReactiveMongoAdapter(CommentReactiveMongoRepository commentRepository) {
+    public CommentReactiveMongoAdapter(CommentReactiveMongoRepository commentRepository, KafkaTemplate<String, Object> template) {
         this.commentRepository = commentRepository;
+        this.template = template;
     }
 
     @Override
     public Mono<CommentCreateResponse> save(Mono<CommentCreateRequest> comment) {
         return comment.map(CommentMapper::createRequestToEntity)
                 .flatMap(commentRepository::insert)
+                .flatMap(savedComment -> sendCommentToTopic(savedComment).thenReturn(savedComment))
                 .map(CommentMapper::entityToCreateResponse);
+    }
+
+    private Mono<Void> sendCommentToTopic(CommentEntity comment) {
+        return Mono.fromFuture(() -> template.send(TOPIC_NAME, comment))
+                .doOnSuccess(sendResult -> {
+                    RecordMetadata metadata = sendResult.getRecordMetadata();
+                    log.info("Comment created and saved successfully. Comment sent id details: [{}], Partition [{}], Offset [{}], Topic [{}]",
+                            comment.getId(),
+                            metadata.partition(),
+                            metadata.offset(),
+                            TOPIC_NAME
+                    );
+                })
+                .doOnError(error -> log.error("Failed to send comment. Comment id details: [{}], Topic [{}], Error message: [{}]",
+                        comment.getId(),
+                        TOPIC_NAME,
+                        error.getMessage()
+                ))
+                .then();
     }
 
     @Override
